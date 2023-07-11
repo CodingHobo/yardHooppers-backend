5f17da5b5b00697e46d1fd4f209ba7454f5bb2a7
Tanya Shylock <tourtanya@gmail.com>
2023-07-10 21:07:09 -0600

    add config for deployment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 /** Shared config for application; can be required many places. */
-// require("dotenv").config();
+require("dotenv").config();
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
